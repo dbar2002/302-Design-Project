a9621712291925aa0bf48016f101003d1ef3d5bd
dbar2002 <90353137+dbar2002@users.noreply.github.com>
2023-02-21 11:41:00 -0800

    Login Feature(still needs editing)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,34 +9,113 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: MyHomePage(),
-      ),
+    return MaterialApp(
+      home: HomePage(),
     );
   }
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
 }
 
-class MyHomePage extends StatelessWidget {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
     return Scaffold(
-      body: Column(
+      body: LoginScreen()
+      );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
+          const Text(
+            "MyApp",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text(
+            "Login to App",
+            style: TextStyle(
+              color: Colors.black, 
+              fontSize: 44, 
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text(
+            "Login to App",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 44,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+            const SizedBox(
+              height: 44,
+            ),
+            const TextField(keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              hintText: "User Email",
+              prefixIcon: Icon(Icons.mail, color: Colors.black),
+            ),
+            ),
+            const SizedBox(
+              height: 26,
+            ),
+            const TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: "User Password",
+              prefixIcon: Icon(Icons.mail, color: Colors.blue),
+            ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            const Text(
+              "Reset your Password",
+              style: TextStyle(color: Colors.blue),
+            ),
+            const SizedBox(
+              height: 88,
+            ),
+            Container(
+              width: double.infinity,
+              child: RawMaterialButton(
+                fillColor: Color(0xFF0069FE),
+                elevation: 0,
+                padding: EdgeInsets.symmetric(vertical: 20),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+                  onPressed: () {},
+                  child: Text("Login",
+                  style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  )),
+              ),
+            ),
         ],
       ),
     );
