25601c117b792e5cab981438f0258dee21ba2d4e
Katelynn El Fattal <katelynn.christinemarie.elfattal@calbaptist.edu>
2023-02-23 11:17:39 -0800

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:avandra/menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,97 +27,25 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LoginScreen()
+      body: MenuScreen()
       );
   }
 }
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class MenuScreen extends StatefulWidget {
+  const MenuScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<MenuScreen> createState() => _MenuScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _MenuScreenState extends State<MenuScreen> {
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            "MyApp",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const Text(
-            "Login to App",
-            style: TextStyle(
-              color: Colors.black, 
-              fontSize: 44, 
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const Text(
-            "Login to App",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 44,
-              fontWeight: FontWeight.bold,
-            ),
-            ),
-            const SizedBox(
-              height: 44,
-            ),
-            const TextField(keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              hintText: "User Email",
-              prefixIcon: Icon(Icons.mail, color: Colors.black),
-            ),
-            ),
-            const SizedBox(
-              height: 26,
-            ),
-            const TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              hintText: "User Password",
-              prefixIcon: Icon(Icons.mail, color: Colors.blue),
-            ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            const Text(
-              "Reset your Password",
-              style: TextStyle(color: Colors.blue),
-            ),
-            const SizedBox(
-              height: 88,
-            ),
-            Container(
-              width: double.infinity,
-              child: RawMaterialButton(
-                fillColor: Color(0xFF0069FE),
-                elevation: 0,
-                padding: EdgeInsets.symmetric(vertical: 20),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12)),
-                  onPressed: () {},
-                  child: Text("Login",
-                  style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  )),
-              ),
-            ),
-        ],
+    return Scaffold(
+      body: Center(
+        child: Menu(),
       ),
     );
   }
