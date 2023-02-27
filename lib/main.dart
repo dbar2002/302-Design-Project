98f405ab38306e790cd7281a3b9be27cb2639a31
Kay Karman <krystalj.karman@calbaptist.edu>
2023-02-26 20:48:09 -0800

    It works im gonna cry

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import '../utils/colors.dart';
 import '../screens/sign_up.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   //This following will be uncommented when we add firebase, but it is here
