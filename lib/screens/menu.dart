b90708e6af525884e5daa6c4773ffe962675d107
katelfa02 <90344082+katelfa02@users.noreply.github.com>
2023-03-01 16:22:13 -0800

    fixed everything, looks ugly

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,76 +1,64 @@
+import 'package:avandra/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class Menu extends StatelessWidget {
+class MenuScreen extends StatefulWidget {
+  const MenuScreen({super.key});
+
+  @override
+  State<MenuScreen> createState() => _MenuScreenState();
+}
+
+class _MenuScreenState extends State<MenuScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-            // shrinkWrap: true,
-            padding: EdgeInsets.all(16),
-            children: [
-              ListTile(
-                contentPadding: EdgeInsets.all(25),
-                leading: Icon(Icons.arrow_back),
-                title: Text('Back'),
-                onTap: () => print('Back'),
-              ),
-              UserAccountsDrawerHeader(  
-                accountName: Text('User Name', style: TextStyle(color: Colors.black54)), 
-                accountEmail: Text('example@universityemail.com', style: TextStyle(color: Colors.black54)),
-                currentAccountPicture: CircleAvatar(
-                  child: ClipOval(
-                    child: Image.network(
-                      'https://oflutter.com/wp-content/uploads/2021/02/girl-profile.png',
-                      width: 90,
-                      height: 90,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  // image: DecorationImage(
-                  //   image: NetworkImage(
-                  //     'https://oflutter.com/wp-content/uploads/2021/02/profile-bg3.jpg'
-                  //   ),
-                  //   fit: BoxFit.cover,
-                  
-                  ),
-                ),
-              ListTile(
-                leading: Icon(Icons.home),
-                title: Text('Home'),
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        elevation: 0,
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(
+                Icons.arrow_back_ios,
+                size: 25,
+                color: Colors.black,
+              )),
+      ),
+      body: 
+      ListView(
+        children: [
+          ListTile(
+                leading: Icon(Icons.home, color: Colors.black),
+                title: Text('Home', style: TextStyle(color: Colors.black)),
                 onTap: () => Navigator.of(context).pushNamedAndRemoveUntil('/home', (route) => false),
-              ),
-              ListTile(
-                leading: Icon(Icons.account_circle),
-                title: Text('Profile'),
-                onTap: () => print('Profile'),
-              ),
-              ListTile(
-                leading: Icon(Icons.push_pin),
-                title: Text('Pins'),
-                onTap: () => print('Pins'),
-              ),          
-              ListTile(
-                leading: Icon(Icons.map_outlined),
-                title: Text('Select Map'),
-                onTap: () => print('Select Map'),
-              ),          
-              Divider(),
-              ListTile(
-                leading: Icon(Icons.logout),
-                title: Text('Log out'),
-                onTap: () => print('Log out'),
-              ),          
-            ],
-        ),
+          ),
+          ListTile(
+            leading: Icon(Icons.account_circle, color: Colors.black),
+            title: Text('Profile', style: TextStyle(color: Colors.black)),
+            onTap: () => print('Profile'),
+          ),
+          ListTile(
+            leading: Icon(Icons.push_pin, color: Colors.black),
+            title: Text('Pins', style: TextStyle(color: Colors.black)),
+            onTap: () => print('Pins'),
+          ),
+          ListTile(
+            leading: Icon(Icons.map_outlined, color: Colors.black),
+            title: Text('Select Map', style: TextStyle(color: Colors.black)),
+            onTap: () => print('Select Map'),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.logout, color: Colors.black),
+            title: Text('Log out', style: TextStyle(color: Colors.black)),
+            onTap: () => print('Log out'),
+          ),
+        ],
+      )
     );
   }
-
-  Widget _buildMenu() {
-    return SafeArea(child: Scaffold(appBar: ),)
-  }
-  //https://stackoverflow.com/questions/62021186/how-do-i-put-a-listview-under-a-scaffold-with-padding
 }
-
