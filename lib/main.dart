3a8872bf397f6adef4554e1dd4d130df22094c77
Katelynn El Fattal <katelynn.christinemarie.elfattal@calbaptist.edu>
2023-03-21 11:35:30 -0700

    added route to selectMap, testing commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:avandra/screens/edit_profile.dart';
 import 'package:avandra/screens/menu.dart';
+import 'package:avandra/screens/select_map.dart';
 import 'package:avandra/screens/sign_in.dart';
 import 'package:avandra/screens/splash_page.dart';
 
@@ -44,6 +45,7 @@ class MyApp extends StatelessWidget {
         '/SignUp': (context) => const SignUpScreen(),
         '/Menu': (context) => const MenuScreen(),
         '/profile': (context) => const ProfilePage(),
+        '/selectMap': (context) => const SelectMapScreen(),
       },
       
 
