98b2f391965481904ab476c994ffb9ce9d8d3aff
Katelynn El Fattal <katelynn.christinemarie.elfattal@calbaptist.edu>
2023-04-04 21:37:00 -0700

    Animated splash screen, more color updates

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -7,10 +7,10 @@ import 'package:flutter/material.dart';
 // const darkerNavigationColor = Color.fromRGBO(51, 51, 51, 1);
 // const reportEventBackgroundColor = Color.fromRGBO(255, 112, 112, 1);
 
-const backgroundColor = Color.fromRGBO(236, 233, 222, 1);
-//const backgroundColor = Color.fromRGBO(235, 244, 247, 1);
+//const backgroundColor = Color.fromRGBO(236, 233, 222, 1);
+const backgroundColor = Color.fromRGBO(236, 234, 228, 1); //slightly lighter, less yellow
 //const splashpageColor = Color.fromRGBO(184, 212, 246, 0.8);
-const splashpageColor = Color.fromRGBO(235, 244, 247, 1);
+const splashpageColor = Color.fromRGBO(235, 244, 247, 1);  //light blue, matches background of new logo
 const buttonColor = Color.fromRGBO(184, 212, 246, 0.8);
 const menuAndNavigationColor = Color.fromRGBO(217, 217, 217, 1);
 const darkerNavigationColor = Color.fromRGBO(51, 51, 51, 1);
