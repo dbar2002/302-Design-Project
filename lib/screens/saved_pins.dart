9e363f691ba2e6bc445efbdb4a5de1e12831b542
dbar2002 <90353137+dbar2002@users.noreply.github.com>
2023-04-05 17:12:05 -0700

    in progress saved pin page xcode files need fixing

---
+++ lib/screens/saved_pins.dart
@@ -0,0 +1,82 @@
+import 'package:avandra/resources/authentication.dart';
+import 'package:avandra/utils/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SavedPinsScreen extends StatefulWidget {
+  const SavedPinsScreen({super.key});
+
+  @override
+  State<SavedPinsScreen> createState() => _SavedPinsScreenState();
+}
+
+class _SavedPinsScreenState extends State<SavedPinsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: backgroundColor,
+        appBar: AppBar(
+          backgroundColor: backgroundColor,
+          elevation: 0,
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(
+                Icons.arrow_back_ios,
+                size: 25,
+                color: Colors.black,
+              )),
+        ),
+        body: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 90.0, top: 170.0),
+          children: [
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Colors.black,
+                size: 30,
+              ),
+              title: Text('Home',
+                  style: TextStyle(color: Colors.black, fontSize: 23)),
+              onTap: () => Navigator.of(context)
+                  .pushNamedAndRemoveUntil('/nav', (route) => false),
+            ),
+            ListTile(
+              leading:
+                  Icon(Icons.account_circle, color: Colors.black, size: 30),
+              title: Text('Profile',
+                  style: TextStyle(color: Colors.black, fontSize: 23)),
+              onTap: () => Navigator.of(context)
+                  .pushNamedAndRemoveUntil('/profile', (route) => false),
+            ),
+            ListTile(
+              leading: Icon(Icons.push_pin, color: Colors.black, size: 30),
+              title: Text('Pins',
+                  style: TextStyle(color: Colors.black, fontSize: 23)),
+              onTap: () => Navigator.of(context)
+                  .pushNamedAndRemoveUntil('/pins', (route) => false),
+            ),
+            ListTile(
+              leading: Icon(Icons.map_outlined, color: Colors.black, size: 30),
+              title: Text('Select Map',
+                  style: TextStyle(color: Colors.black, fontSize: 23)),
+              onTap: () => Navigator.of(context)
+                  .pushNamedAndRemoveUntil('/selectMap', (route) => false),
+            ),
+            Divider(
+              color: Colors.black,
+              endIndent: 90,
+            ),
+            ListTile(
+              leading: Icon(Icons.logout, color: Colors.black, size: 30),
+              title: Text('Log out',
+                  style: TextStyle(color: Colors.black, fontSize: 23)),
+              onTap: () => AuthMethods().signOut(),
+            ),
+          ],
+        ));
+  }
+}
